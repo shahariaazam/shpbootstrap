119f9dc4e4052514884b01bd287df6d2595c0413
Shaharia Azam <shaharia.azam@gmail.com>
2012-12-25 04:05:52 +0600

    Add Bootstrap default assets

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 define('CLASS_DIR','classes');
+define('CSS_DIR','assets/css');
+define('JS_DIR','assets/js');
+define('IMG_DIR','assets/img');
 require_once(CLASS_DIR.'/Bootstrap.php');
+require_once(CSS_DIR.'/bootstrap.css');
+
 $Bootstrap=new Bootstrap();
 echo $Bootstrap->name;
---
+++ pages/inxed.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Speed Plus Network
+ * Date: 12/25/12
+ * Time: 3:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
