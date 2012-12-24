a3bc3fe1162a4eee6bbcb8dd6cda1c6bf532f8e2
Shaharia Azam <shaharia.azam@gmail.com>
2012-12-25 03:58:53 +0600

    Initialization beta

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Shaharia Azam
+ * @Author URL: http://www.shahariaazam.com
+ * @Project: Bootstrap CSS Framework PHP Helper
+ * @Version: 1.0
+ * @Project URL: http://www.shahariaazam.com
+ * @Description: Bootstrap twitter CSS framework handle through PHP component
+ */
